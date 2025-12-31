518ceacced197178c722e05f32c9eaf3a9948d2b
Mike McKerns <mmckerns@caltech.edu>
2025-12-30 22:44:34 -0500

    update copyright for 2026, urllib3 to 2.6.0 (#58)

--- pygrace/extensions/colorbar.py
+++ pygrace/extensions/colorbar.py
@@ -3,7 +3,7 @@
 # Author: Mike McKerns (mmckerns @uqfoundation)
 # Author: Daniel Stouffer (daniel @stoufferlab.org)
 # Copyright (c) 2013 Daniel Stouffer.
-# Copyright (c) 2023-2025 The Uncertainty Quantification Foundation.
+# Copyright (c) 2023-2026 The Uncertainty Quantification Foundation.
 # License: 3-clause BSD.  The full license text is available at:
 #  - https://github.com/uqfoundation/pygrace/blob/master/LICENSE
 #
